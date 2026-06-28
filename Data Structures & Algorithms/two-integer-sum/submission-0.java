8c66c1ca6f9ead6262c133bfe4b31f2bad4dc007
Vaishnavi M <vaishnavim1105@gmail.com>
2026-06-28 22:11:32 +0530

    brute force
    Refactored twoSum method to use a nested loop approach instead of a HashMap for finding two indices that sum to the target.

--- Data Structures & Algorithms/two-integer-sum/submission-0.java
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -1,16 +1,12 @@
-class Solution {
+public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int[] res= new int[2];
-        for(int i=0;i<nums.length;i++){
-            int diff=target-nums[i];
-            if(map.containsKey(diff)){
-                res[0]=map.get(diff);
-                res[1]=i;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
             }
-            map.put(nums[i],i);
         }
-        Arrays.sort(res);
-        return res;
+        return new int[0];
     }
 }
